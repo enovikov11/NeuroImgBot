6a50e60085371f94a853bae2fdea0722245be760
Evgenii Novikov <enovikov11@yandex.ru>
2022-09-11 22:49:30 +0300

    pre

--- worker/pre.py
+++ worker/pre.py
@@ -11,5 +11,3 @@ os.system("git pull")
 print("Git pulled " + str(time.time()))
 os.system(f"sshfs -o reconnect {secrets['HOST']}:/home/enovikov11/models-ramdisk /home/enovikov11/.cache/huggingface/diffusers")
 print("sshfs mounted " + str(time.time()))
-os.system("/home/enovikov11/anaconda3/envs/neuroimgbot/bin/python3.8 /home/enovikov11/NeuroImgBot/worker/runner.py")
-
