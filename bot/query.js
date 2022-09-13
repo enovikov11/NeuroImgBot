97e6cda8a900adcb640e532bc64acad2dfa8e57c
Evgenii Novikov <enovikov11@yandex.ru>
2022-09-13 23:36:40 +0300

    command

--- bot/query.js
+++ bot/query.js
@@ -1,5 +1,5 @@
 function parseRequest(requestText, hasPhoto, isReply) {
-    if (!requestText || (!/@neuroimgbot/i.test(requestText) && !isReply)) {
+    if (!requestText || (!/(\/r)?@neuroimgbot/i.test(requestText) && !isReply)) {
         return null;
     }
 
@@ -14,12 +14,16 @@ function parseRequest(requestText, hasPhoto, isReply) {
         scale = getParam(/\bscale=(\d+(\.\d+)?)\b/, 2, 7.5, 20),
         steps = getParam(/\bsteps=(\d+)\b/, 20, 50, 500),
         request = requestText
-            .replace(/@neuroimgbot/i, '')
+            .replace(/(\/r)?@neuroimgbot/i, '')
             .replace(/\b\w+=[\w.]+\b/g, '')
             .replace(/\n/g, ' ')
             .replace(/[ ]+/g, ' ')
             .replace(/^ /, '')
             .replace(/ $/, '');
+
+    if (!request.length) {
+        return null;
+    }
 
     count = Math.floor(Math.min(500, count * steps) / steps);
 
